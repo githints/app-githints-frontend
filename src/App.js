c4ed7d5b7fba734218db6356c89bafe11c8adb1e
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2018-06-26 23:27:39 +0200

    refactor(App): simple page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './App.css';
+import {Header, Search, Wrapper, Container} from './common';
 
-const Header = ({title}) => (
-  <h1>{title}</h1>
-);
+function logEvent(e) {
+  console.log(e);
+}
+
+function logSuggestion(e) {
+  console.log(e);
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {suggestions: ['ciao', 'prova', 'test', 'testooo', 'ciaone', 'pippo']};
+  }
+
   render() {
     return (
-      <Header title={'Welcome to Githints'} />
+      <div className="App">
+        <Header/>
+        <Wrapper className="App__hero">
+          <Container>
+            <h2>My <b>super</b> slogan GitHint</h2>
+            <Search onChange={logEvent} onChoose={logSuggestion} suggestions={this.state.suggestions}/>
+          </Container>
+        </Wrapper>
+      </div>
     );
   }
 }
